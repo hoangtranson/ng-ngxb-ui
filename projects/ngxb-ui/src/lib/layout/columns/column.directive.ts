47f428063dcfc8199b3009a8ed33bbb830e3585a
Tran Son Hoang <hayden.tran.d@gmail.com>
2019-11-20 14:48:27 +0800

    feat(ngxb-columns): support offset

--- projects/ngxb-ui/src/lib/layout/columns/column.directive.ts
+++ projects/ngxb-ui/src/lib/layout/columns/column.directive.ts
@@ -16,6 +16,21 @@ const SIZE_ATTRIBUTES = [
   'w-12'
 ];
 
+const OFFSET_ATTRIBUTES = [
+  'offset-1',
+  'offset-2',
+  'offset-3',
+  'offset-4',
+  'offset-5',
+  'offset-6',
+  'offset-7',
+  'offset-8',
+  'offset-9',
+  'offset-10',
+  'offset-11',
+  'offset-12'
+];
+
 @Directive({
   selector: '[ngxb-column]'
 })
@@ -30,6 +45,11 @@ export class ColumnDirective extends ClassBaseComponent {
         (this._getHostElement() as HTMLElement).classList.add(`is-${temp[1]}`);
       }
     }
-  }
 
+    for (const attr of OFFSET_ATTRIBUTES) {
+      if (this._hasHostAttributes(attr)) {
+        (this._getHostElement() as HTMLElement).classList.add(`is-${attr}`);
+      }
+    }
+  }
 }
